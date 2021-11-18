c7f3e3aecf58900ca713de119265d02c9efb44c7
smarcet <smarcet@gmail.com>
2021-11-18 14:16:11 -0300

    Rollback with credentials change
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components/forms/event-material-form.js
+++ src/components/forms/event-material-form.js
@@ -233,7 +233,6 @@ class EventMaterialForm extends React.Component {
                             mediaType={slideMediaType}
                             postUrl={`${window.API_BASE_URL}/api/public/v1/files/upload`}
                             error={hasErrors("slide", errors)}
-                            djsConfig={{withCredentials:true}}
                         />
                     </div>
                     <div className="col-md-7 text-center">
@@ -293,7 +292,6 @@ class EventMaterialForm extends React.Component {
                             mediaType={media_type}
                             postUrl={`${window.API_BASE_URL}/api/public/v1/files/upload`}
                             error={hasErrors(media_type.name, errors)}
-                            djsConfig={{withCredentials:true}}
                         />
                     </div>
                 </div>
