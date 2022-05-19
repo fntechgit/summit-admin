a2d0d27c4f727be49a1e1da731eaab09de60ed15
smarcet <smarcet@gmail.com>
2022-05-19 12:33:32 -0300

    Upgraded uicore version
    Signed-off-by: smarcet <smarcet@gmail.com>

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const TerserJSPlugin            = require('terser-webpack-plugin');
 const MiniCssExtractPlugin      = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin   = require('optimize-css-assets-webpack-plugin');
-const { merge }                     = require('webpack-merge');
+const { merge }                 = require('webpack-merge');
 const common                    = require('./webpack.common.js');
 const path                      = require('path');
 
