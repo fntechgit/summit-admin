9a314a6d9d2008596254af58d56c78306194401b
Tomás Castillo <tcastilloboireau@gmail.com>
2022-11-10 12:03:56 -0300

    New general sponsorships and summit sponsorship types
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/pages/sponsors/edit-summit-sponsorship-page.js
+++ src/pages/sponsors/edit-summit-sponsorship-page.js
@@ -23,7 +23,6 @@ import {
     uploadSponsorshipBadgeImage, 
     removeSponsorshipBadgeImage
 } from "../../actions/sponsor-actions";
-import { getSponsorships } from '../../actions/sponsorship-actions';
 
 class EditSummitSponsorshipPage extends React.Component {
 
@@ -36,8 +35,6 @@ class EditSummitSponsorshipPage extends React.Component {
         } else {
             props.getSummitSponsorship(sponsorshipId);
         }
-
-        props.getSponsorships();
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -90,6 +87,7 @@ export default connect (
     {
         getSummitSponsorship,
         getSponsorships,
+        getSummitById,
         resetSummitSponsorshipForm,
         saveSummitSponsorship,
         uploadSponsorshipBadgeImage,
