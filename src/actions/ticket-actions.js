849a8d56378452ca315256972b13a447eb6e3d00
Tomás Castillo <tcastilloboireau@gmail.com>
2024-03-08 11:44:03 -0300

    Add missing expands, re order columns
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/actions/ticket-actions.js
+++ src/actions/ticket-actions.js
@@ -439,7 +439,7 @@ export const getTicket = (ticketId) => async (dispatch, getState) => {
 
     const params = {
         access_token: accessToken,
-        expand: 'badge,badge.features,promo_code,ticket_type,owner,owner.member,refund_requests,refund_requests.requested_by,refund_requests.action_by'
+        expand: 'badge,badge.features,promo_code,ticket_type,owner,owner.member,refund_requests,refund_requests.requested_by,refund_requests.action_by,refund_requests.refunded_taxes,refund_requests.refunded_taxes.tax'
     };
 
     return getRequest(
