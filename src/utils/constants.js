3c19d704b64f577f9dfd91f22ce2dd9567c733bb
Tomás Castillo <tcastilloboireau@gmail.com>
2024-07-18 20:53:58 -0300

    Change textarea input for text area with char count
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -73,3 +73,7 @@ export const DEFAULT_CURRENT_PAGE = 1;
 export const DEFAULT_PER_PAGE = 10;
 
 export const DEFAULT_ORDER_DIR = 1;
+
+export const MILLISECONDS_TO_SECONDS = 1000;
+
+export const INDEX_NOT_FOUND = -1;
