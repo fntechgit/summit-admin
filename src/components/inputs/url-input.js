9a1031bc707a8ad76a44e7ded47a5df543017b0a
Tomás Castillo <tcastilloboireau@gmail.com>
2024-09-09 18:35:39 -0300

    Adjust inputs, and texts, new input for url
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/components/inputs/url-input.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2019 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+import React, { useState, useEffect } from "react";
+import { Input } from "openstack-uicore-foundation/lib/components";
+
+const UrlInput = function ({ onChange, error, id, value: propValue, ...rest }) {
+  const [stateValue, setStateValue] = useState(propValue || "");
+  const [invalidUrl, setInvalidUrl] = useState("");
+
+  useEffect(() => {
+    setStateValue(propValue || "");
+  }, [propValue]);
+
+  const handleValidation = (ev) => {
+    let { value, id } = ev.target;
+
+    // Remove all whitespaces and trailing slash
+    value = value.replace(/\s+|\/$/g, "");
+    const urlPattern = /^(https?:\/\/)/;
+    setStateValue(value);
+
+    if (!urlPattern.test(value)) {
+      setInvalidUrl("URL must start with http:// or https://");
+      return;
+    }
+
+    setInvalidUrl("");
+
+    onChange({
+      target: {
+        value,
+        id
+      }
+    });
+  };
+
+  return (
+    <Input
+      id={id}
+      value={stateValue}
+      onChange={handleValidation}
+      error={invalidUrl || error}
+      {...rest}
+    />
+  );
+};
+
+export default UrlInput;
