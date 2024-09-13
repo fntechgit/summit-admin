0defb6f6c726e0250b7a9a588baa5a95e8fc1d02
Santiago Palenque <santipalenque@gmail.com>
2024-09-13 18:33:24 -0300

    WIP

--- src/components/tables/room-occupancy-table/OccupancyActionsTableCell.js
+++ src/components/tables/room-occupancy-table/OccupancyActionsTableCell.js
@@ -1,42 +1,52 @@
 import React from "react";
 
-export default class OccupancyActionsTableCell extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function OccupancyActionsTableCell({ actions, id, value }) {
+  const style = {
+    marginLeft: "10px",
+    marginRight: "10px",
+    width: "103px",
+    display: "inline-block",
+    textAlign: "center"
+  };
 
-  render() {
-    const { actions, id, value } = this.props;
+  const lessDisable = value === "EMPTY";
+  const moreDisable = value === "OVERFLOW";
 
-    const style = {
-      marginLeft: "10px",
-      marginRight: "10px",
-      width: "90px",
-      display: "inline-block",
-      textAlign: "center"
-    };
-
-    const lessDisable = value === "EMPTY";
-    const moreDisable = value === "OVERFLOW";
-
-    return (
-      <td className="actions" key="actions">
+  const state = () => {
+    if (value === "OVERFLOW" && actions.setOverflowStream) {
+      return (
         <button
-          className="btn btn-default"
-          onClick={actions.onLess.bind(this, id)}
-          disabled={lessDisable}
+          className="btn btn-danger"
+          style={{ margin: "0 10px" }}
+          onClick={actions.setOverflowStream}
         >
-          <i className="fa fa-minus" />
+          OVERFLOW
         </button>
-        <span style={style}>{value}</span>
-        <button
-          className="btn btn-default"
-          onClick={actions.onMore.bind(this, id)}
-          disabled={moreDisable}
-        >
-          <i className="fa fa-plus" />
-        </button>
-      </td>
-    );
-  }
+      );
+    }
+
+    return <span style={style}>{value}</span>;
+  };
+
+  return (
+    <td className="actions" key="actions">
+      <button
+        className="btn btn-default"
+        onClick={actions.onLess.bind(this, id)}
+        disabled={lessDisable}
+      >
+        <i className="fa fa-minus" />
+      </button>
+      {state()}
+      <button
+        className="btn btn-default"
+        onClick={actions.onMore.bind(this, id)}
+        disabled={moreDisable}
+      >
+        <i className="fa fa-plus" />
+      </button>
+    </td>
+  );
 }
+
+export default OccupancyActionsTableCell;
--- src/components/tables/room-occupancy-table/OccupancyTable.js
+++ src/components/tables/room-occupancy-table/OccupancyTable.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import T from "i18n-react";
 import OccupancyTableHeading from "./OccupancyTableHeading";
 import OccupancyTableCell from "./OccupancyTableCell";
 import OccupancyTableRow from "./OccupancyTableRow";
@@ -15,12 +17,11 @@ const defaults = {
 };
 
 const createRow = (row, columns, actions) => {
-  var action_buttons = "";
-  var cells = columns.map((col, i) => {
-    let colClass = col.hasOwnProperty("className") ? col.className : "";
+  const cells = columns.map((col, i) => {
+    const colClass = col.hasOwnProperty("className") ? col.className : "";
 
     return (
-      <OccupancyTableCell key={"cell_" + i} className={colClass}>
+      <OccupancyTableCell key={`cell_${i}`} className={colClass}>
         {row[col.columnKey]}
       </OccupancyTableCell>
     );
@@ -30,7 +31,7 @@ const createRow = (row, columns, actions) => {
     cells.push(
       <OccupancyActionsTableCell
         key="actions_cell"
-        id={row["id"]}
+        id={row.id}
         value={row[actions.valueRow]}
         actions={actions}
       />
@@ -50,33 +51,23 @@ const getSortDir = (columnKey, columnIndex, sortCol, sortDir) => {
   return null;
 };
 
-const OccupancyTable = (props) => {
-  let { options, columns } = props;
+function OccupancyTable(props) {
+  const [showOFModal, setShowOFModal] = useState(false);
+  const { options, columns } = props;
+  const sortCol = options?.sortCol || defaults.sortCol;
+  const sortDir = options?.sortDir || defaults.sortDir;
+  const sortFunc = options?.sortFunc || defaults.sortFunc;
+  options.actions.setOverflowStream = () => setShowOFModal(true);
 
   return (
     <div className="occupancyTableWrapper">
-      <table className={"table table-striped table-hover occupancyTable"}>
+      <table className="table table-striped table-hover occupancyTable">
         <thead>
           <tr>
             {columns.map((col, i) => {
-              let sortCol = options.hasOwnProperty("sortCol")
-                ? options.sortCol
-                : defaults.sortCol;
-              let sortDir = options.hasOwnProperty("sortDir")
-                ? options.sortDir
-                : defaults.sortDir;
-              let sortFunc = options.hasOwnProperty("sortFunc")
-                ? options.sortFunc
-                : defaults.sortFunc;
-              let sortable = col.hasOwnProperty("sortable")
-                ? col.sortable
-                : defaults.sortable;
-              let colWidth = col.hasOwnProperty("width")
-                ? col.width
-                : defaults.colWidth;
-              let colClass = col.hasOwnProperty("className")
-                ? col.className
-                : "";
+              const sortable = col?.sortable || defaults.sortable;
+              const colWidth = col?.width || defaults.colWidth;
+              const colClass = col?.className || "";
 
               return (
                 <OccupancyTableHeading
@@ -88,7 +79,7 @@ const OccupancyTable = (props) => {
                   columnIndex={i}
                   columnKey={col.columnKey}
                   width={colWidth}
-                  key={"heading_" + i}
+                  key={`heading_${i}`}
                 >
                   {col.value}
                 </OccupancyTableHeading>
@@ -108,19 +99,34 @@ const OccupancyTable = (props) => {
                 console.warn(
                   `Data at row ${i} is ${row.length}. It should be ${columns.length}.`
                 );
-                return <tr key={"row_" + i} />;
+                return <tr key={`row_${i}`} />;
               }
 
               return (
-                <OccupancyTableRow even={i % 2 === 0} key={"row_" + i}>
+                <OccupancyTableRow even={i % 2 === 0} key={`row_${i}`}>
                   {createRow(row, columns, options.actions)}
                 </OccupancyTableRow>
               );
             })}
         </tbody>
       </table>
+      <Modal show={showOFModal} onHide={() => setShowOFModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Overflow Stream</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ overflow: "auto", maxHeight: "75vh" }}>
+          <div className="row">
+            <div className="col-md-12">STREAM</div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-primary" onClick={console.log}>
+            {T.translate("general.save")}
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
-};
+}
 
 export default OccupancyTable;
