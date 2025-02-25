6d9d50c2d87a57628b572a468a543ebabb49f836
Tomás Castillo <tomrndom@users.noreply.github.com>
2025-02-25 15:19:14 -0300

    fix: add ticket type input on promocodes and attendee ticket modal (#554)
    * Add ticket type input on promocodes and attendee ticket modal
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * clean console logs, move logic from action to form
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    ---------
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/actions/promocode-actions.js
+++ src/actions/promocode-actions.js
@@ -240,7 +240,7 @@ export const getPromocode = (promocodeId) => async (dispatch, getState) => {
 
   const params = {
     expand:
-      "owner,sponsor,sponsor.company,sponsor.sponsorship,sponsor.sponsorship.type,speaker,tickets,ticket_type,ticket_types_rules,tags",
+      "owner,sponsor,sponsor.company,sponsor.sponsorship,sponsor.sponsorship.type,speaker,tickets,ticket_type,ticket_types_rules,tags,allowed_ticket_types",
     access_token: accessToken
   };
 
@@ -270,7 +270,7 @@ export const savePromocode =
 
     const params = {
       expand:
-        "owner,sponsor,sponsor.company,sponsor.sponsorship,speaker,tickets,ticket_type,ticket_types_rules,tags",
+        "owner,sponsor,sponsor.company,sponsor.sponsorship,speaker,tickets,ticket_type,ticket_types_rules,tags,allowed_ticket_types",
       access_token: accessToken
     };
 
