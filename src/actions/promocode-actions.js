8f5c57f0f0d87fbbcf088695ce133dbcd737b582
Santi <santipalenque@gmail.com>
2025-04-25 15:54:25 -0300

    fix: improve promocodes api call (#580)
    * remove owners from payload
    
    * remove speaker and tickets

--- src/actions/promocode-actions.js
+++ src/actions/promocode-actions.js
@@ -240,7 +240,8 @@ export const getPromocode = (promocodeId) => async (dispatch, getState) => {
 
   const params = {
     expand:
-      "owner,sponsor,sponsor.company,sponsor.sponsorship,sponsor.sponsorship.type,speaker,tickets,ticket_type,ticket_types_rules,tags,allowed_ticket_types",
+      "owner,sponsor,sponsor.company,sponsor.sponsorship,sponsor.sponsorship.type,ticket_type,ticket_types_rules,tags,allowed_ticket_types",
+    relations: "owner,sponsor,ticket_types_rules,tags,allowed_ticket_types",
     access_token: accessToken
   };
 
