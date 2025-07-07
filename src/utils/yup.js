8701532365fcfa9f8b06d01c449e888202bda708
Santiago Palenque <santipalenque@gmail.com>
2025-07-07 17:38:48 -0300

    feat: use ; as email separator

--- src/utils/yup.js
+++ src/utils/yup.js
@@ -7,7 +7,7 @@ export const addEmailListValidator = () => {
       if (!value) {
         return true; // Or handle empty values as needed
       }
-      const emails = value.replace(/\s/g, "").split(",");
+      const emails = value.replace(/\s/g, "").split(";");
       const isValid = emails.every((email) =>
         /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
       );
