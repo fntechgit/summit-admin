0dc732f31f630dc149e05ff795ae2ae4714821e5
Tomás Castillo <tcastilloboireau@gmail.com>
2025-07-10 02:50:54 -0300

    add new popup for new summit tiers
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -129,6 +129,8 @@ export const HEX_RADIX = 16;
 
 export const DEBOUNCE_WAIT = 500;
 
+export const DEBOUNCE_WAIT_250 = 250;
+
 export const LETTERS_IN_ALPHABET = 26;
 
 export const UPPERCASE_A_IN_ASCII = 65;
