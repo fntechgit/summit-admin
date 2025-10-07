75d1366cbe784d6263ac66d48f988c44ae117186
Santiago Palenque <santipalenque@gmail.com>
2025-10-07 10:40:09 -0300

    feat: new room manifest report

--- src/layouts/reports-layout.js
+++ src/layouts/reports-layout.js
@@ -1,4 +1,4 @@
-/**
+/* *
  * Copyright 2017 OpenStack Foundation
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,163 +9,165 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/
+ * */
 
 import React from "react";
-import { Switch, Route, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import T from "i18n-react/dist/i18n-react";
 import { Breadcrumb } from "react-breadcrumbs";
 import Restrict from "../routes/restrict";
 import ReportListPage from "../pages/reports/report-list-page";
 import NoMatchPage from "../pages/no-match-page";
 import {
-  RsvpReport,
-  RsvpEventReport,
-  RoomReport,
-  PresentationVideoReport,
-  FeedbackReport,
+  AttendeeReport,
   FeedbackGroupReport,
-  TagReport,
-  SingleTagReport,
-  SmartSpeakerReport,
-  SmartPresentationReport,
+  FeedbackReport,
   MetricsReport,
-  AttendeeReport
+  PresentationVideoReport,
+  RoomReport,
+  RoomManifestReport,
+  RsvpEventReport,
+  RsvpReport,
+  SingleTagReport,
+  SmartPresentationReport,
+  SmartSpeakerReport,
+  TagReport
 } from "../components/reports";
 
-class ReportsLayout extends React.Component {
-  render() {
-    const { match } = this.props;
-    return (
-      <div>
-        <Breadcrumb
-          data={{ title: T.translate("reports.reports"), pathname: match.url }}
-        />
+const ReportsLayout = ({ match }) => (
+  <div>
+    <Breadcrumb
+      data={{ title: T.translate("reports.reports"), pathname: match.url }}
+    />
 
-        <Switch>
-          <Route strict exact path={match.url} component={ReportListPage} />
-          <Route
-            path={`${match.url}/rsvp_report`}
-            render={(props) => (
-              <div>
-                <Breadcrumb
-                  data={{
-                    title: T.translate("reports.rsvp_report"),
-                    pathname: match.url
-                  }}
-                />
-                <Switch>
-                  <Route
-                    strict
-                    exact
-                    path={`${props.match.url}`}
-                    component={RsvpReport}
-                  />
-                  <Route
-                    strict
-                    exact
-                    path={`${props.match.url}/:event_id(\\d+)`}
-                    component={RsvpEventReport}
-                  />
-                </Switch>
-              </div>
-            )}
-          />
-          <Route
-            strict
-            exact
-            path={`${match.url}/room_report`}
-            component={RoomReport}
-          />
-          <Route
-            strict
-            exact
-            path={`${match.url}/presentation_video_report`}
-            component={PresentationVideoReport}
-          />
-          <Route
-            path={`${match.url}/feedback_report`}
-            render={(props) => (
-              <div>
-                <Breadcrumb
-                  data={{
-                    title: T.translate("reports.feedback_report"),
-                    pathname: match.url
-                  }}
-                />
-                <Switch>
-                  <Route
-                    strict
-                    exact
-                    path={`${props.match.url}`}
-                    component={FeedbackReport}
-                  />
-                  <Route
-                    strict
-                    exact
-                    path={`${props.match.url}/:group(track|speaker|presentation)/:group_id(\\d+)`}
-                    component={FeedbackGroupReport}
-                  />
-                </Switch>
-              </div>
-            )}
-          />
-          <Route
-            path={`${match.url}/tag_report`}
-            render={(props) => (
-              <div>
-                <Breadcrumb
-                  data={{
-                    title: T.translate("reports.tag_report"),
-                    pathname: match.url
-                  }}
-                />
-                <Switch>
-                  <Route
-                    strict
-                    exact
-                    path={`${props.match.url}`}
-                    component={TagReport}
-                  />
-                  <Route
-                    strict
-                    exact
-                    path={`${props.match.url}/:tag_id(\\d+)`}
-                    component={SingleTagReport}
-                  />
-                </Switch>
-              </div>
-            )}
-          />
-          <Route
-            strict
-            exact
-            path={`${match.url}/speaker_report`}
-            component={SmartSpeakerReport}
-          />
-          <Route
-            strict
-            exact
-            path={`${match.url}/presentation_report`}
-            component={SmartPresentationReport}
-          />
-          <Route
-            strict
-            exact
-            path={`${match.url}/metrics_report`}
-            component={MetricsReport}
-          />
-          <Route
-            strict
-            exact
-            path={`${match.url}/attendee_report`}
-            component={AttendeeReport}
-          />
-          <Route component={NoMatchPage} />
-        </Switch>
-      </div>
-    );
-  }
-}
+    <Switch>
+      <Route strict exact path={match.url} component={ReportListPage} />
+      <Route
+        path={`${match.url}/rsvp_report`}
+        render={(props) => (
+          <div>
+            <Breadcrumb
+              data={{
+                title: T.translate("reports.rsvp_report"),
+                pathname: match.url
+              }}
+            />
+            <Switch>
+              <Route
+                strict
+                exact
+                path={`${props.match.url}`}
+                component={RsvpReport}
+              />
+              <Route
+                strict
+                exact
+                path={`${props.match.url}/:event_id(\\d+)`}
+                component={RsvpEventReport}
+              />
+            </Switch>
+          </div>
+        )}
+      />
+      <Route
+        strict
+        exact
+        path={`${match.url}/room_metrics_report`}
+        component={RoomReport}
+      />
+      <Route
+        strict
+        exact
+        path={`${match.url}/room_manifest_report`}
+        component={RoomManifestReport}
+      />
+      <Route
+        strict
+        exact
+        path={`${match.url}/presentation_video_report`}
+        component={PresentationVideoReport}
+      />
+      <Route
+        path={`${match.url}/feedback_report`}
+        render={(props) => (
+          <div>
+            <Breadcrumb
+              data={{
+                title: T.translate("reports.feedback_report"),
+                pathname: match.url
+              }}
+            />
+            <Switch>
+              <Route
+                strict
+                exact
+                path={`${props.match.url}`}
+                component={FeedbackReport}
+              />
+              <Route
+                strict
+                exact
+                path={`${props.match.url}/:group(track|speaker|presentation)/:group_id(\\d+)`}
+                component={FeedbackGroupReport}
+              />
+            </Switch>
+          </div>
+        )}
+      />
+      <Route
+        path={`${match.url}/tag_report`}
+        render={(props) => (
+          <div>
+            <Breadcrumb
+              data={{
+                title: T.translate("reports.tag_report"),
+                pathname: match.url
+              }}
+            />
+            <Switch>
+              <Route
+                strict
+                exact
+                path={`${props.match.url}`}
+                component={TagReport}
+              />
+              <Route
+                strict
+                exact
+                path={`${props.match.url}/:tag_id(\\d+)`}
+                component={SingleTagReport}
+              />
+            </Switch>
+          </div>
+        )}
+      />
+      <Route
+        strict
+        exact
+        path={`${match.url}/speaker_report`}
+        component={SmartSpeakerReport}
+      />
+      <Route
+        strict
+        exact
+        path={`${match.url}/presentation_report`}
+        component={SmartPresentationReport}
+      />
+      <Route
+        strict
+        exact
+        path={`${match.url}/metrics_report`}
+        component={MetricsReport}
+      />
+      <Route
+        strict
+        exact
+        path={`${match.url}/attendee_report`}
+        component={AttendeeReport}
+      />
+      <Route component={NoMatchPage} />
+    </Switch>
+  </div>
+);
 
 export default Restrict(withRouter(ReportsLayout), "reports");
