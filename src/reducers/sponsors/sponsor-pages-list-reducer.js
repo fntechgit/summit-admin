04d7148f7a6bcbbc32b8876fae615b75d589b542
Tomás Castillo <tcastilloboireau@gmail.com>
2025-12-16 17:06:53 -0300

    feat: add new menu and page for summit sponsor pages, page template popup
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

---
+++ src/reducers/sponsors/sponsor-pages-list-reducer.js
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2019 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+import { LOGOUT_USER } from "openstack-uicore-foundation/lib/security/actions";
+import {
+  RECEIVE_SPONSOR_PAGES,
+  REQUEST_SPONSOR_PAGES
+} from "../../actions/sponsor-pages-actions";
+import { SET_CURRENT_SUMMIT } from "../../actions/summit-actions";
+
+const DEFAULT_STATE = {
+  sponsorPages: [],
+  term: "",
+  order: "name",
+  orderDir: 1,
+  currentPage: 1,
+  lastPage: 1,
+  perPage: 10,
+  totalCount: 0,
+  hideArchived: false
+};
+
+const sponsorPagesListReducer = (state = DEFAULT_STATE, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case SET_CURRENT_SUMMIT:
+    case LOGOUT_USER: {
+      return DEFAULT_STATE;
+    }
+    case REQUEST_SPONSOR_PAGES: {
+      const { order, orderDir, page, term, hideArchived } = payload;
+
+      return {
+        ...state,
+        order,
+        orderDir,
+        sponsorPages: [],
+        currentPage: page,
+        term,
+        hideArchived
+      };
+    }
+    case RECEIVE_SPONSOR_PAGES: {
+      const {
+        current_page: currentPage,
+        total,
+        last_page: lastPage
+      } = payload.response;
+
+      const sponsorPages = payload.response.data.map((a) => ({
+        id: a.id,
+        code: a.code,
+        name: a.name,
+        tier: a.sponsorship_types.map((s) => s.name).join(", "),
+        info_mod: a.modules.filter((m) => m.kind === "Info").length,
+        upload_mod: a.modules.filter((m) => m.kind === "Upload").length,
+        download_mod: a.modules.filter((m) => m.kind === "Download").length,
+        is_archived: a.is_archived
+      }));
+
+      return {
+        ...state,
+        sponsorPages,
+        currentPage,
+        totalCount: total,
+        lastPage
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default sponsorPagesListReducer;
