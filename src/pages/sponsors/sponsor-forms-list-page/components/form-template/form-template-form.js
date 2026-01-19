85c08aab14b6e712c5fb88a63d0c65b1c1b0409e
Niko <nicolas.olmos@exomindset.co>
2026-01-19 14:10:51 -0300

    86b7gyddp - fix: add asterisks (#741)
    * fix: add asterisks
    
    * chore: use required prop to show asterisk

--- src/pages/sponsors/sponsor-forms-list-page/components/form-template/form-template-form.js
+++ src/pages/sponsors/sponsor-forms-list-page/components/form-template/form-template-form.js
@@ -115,19 +115,19 @@ const FormTemplateForm = ({
             <Grid2 size={4}>
               <MuiFormikDatepicker
                 name="opens_at"
+                required
                 label={T.translate(
                   "sponsor_forms.form_template_popup.opens_at"
                 )}
-                required
               />
             </Grid2>
             <Grid2 size={4}>
               <MuiFormikDatepicker
+                required
                 name="expires_at"
                 label={T.translate(
                   "sponsor_forms.form_template_popup.expires_at"
                 )}
-                required
               />
             </Grid2>
             <Grid2 size={12}>
