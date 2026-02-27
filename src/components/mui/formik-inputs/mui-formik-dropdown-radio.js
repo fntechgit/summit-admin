ac7e9fd6952e0cb712b7c17960981ff06caa9930
Santiago Palenque <santipalenque@gmail.com>
2026-02-27 12:40:07 -0300

    fix: adding headers

--- src/components/mui/formik-inputs/mui-formik-dropdown-radio.js
+++ src/components/mui/formik-inputs/mui-formik-dropdown-radio.js
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
 import React from "react";
 import {
   FormControl,
@@ -10,7 +23,8 @@ import { useField } from "formik";
 import T from "i18n-react/dist/i18n-react";
 
 const MuiFormikDropdownRadio = ({ name, options, placeholder, ...rest }) => {
-  const finalPlaceholder = placeholder || T.translate("general.select_an_option");
+  const finalPlaceholder =
+    placeholder || T.translate("general.select_an_option");
   const [field, meta, helpers] = useField(name);
 
   const handleChange = (event) => {
