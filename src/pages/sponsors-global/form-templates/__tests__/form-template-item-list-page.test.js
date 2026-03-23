51348395d6459291c2b5ff67e27e9136350cfef2
Priscila Moneo <priscila_moneo@hotmail.com.ar>
2026-03-23 17:38:02 -0300

    fix: fix pagination issue page not found (#822)

---
+++ src/pages/sponsors-global/form-templates/__tests__/form-template-item-list-page.test.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { waitFor } from "@testing-library/react";
+import FormTemplateItemListPage from "../form-template-item-list-page";
+import { renderWithRedux } from "../../../../utils/test-utils";
+import { getFormTemplateItems } from "../../../../actions/form-template-item-actions";
+import { getFormTemplate } from "../../../../actions/form-template-actions";
+import { DEFAULT_CURRENT_PAGE } from "../../../../utils/constants";
+
+jest.mock("../../../../actions/form-template-item-actions", () => ({
+  ...jest.requireActual("../../../../actions/form-template-item-actions"),
+  getFormTemplateItems: jest.fn(() => () => Promise.resolve())
+}));
+
+jest.mock("../../../../actions/form-template-actions", () => ({
+  ...jest.requireActual("../../../../actions/form-template-actions"),
+  getFormTemplate: jest.fn(() => () => Promise.resolve())
+}));
+
+describe("FormTemplateItemListPage", () => {
+  const formTemplateId = 123;
+  const initialPage = 2;
+  const perPage = 10;
+  const order = "name";
+  const orderDir = 1;
+  const hideArchived = false;
+  const buildInitialState = () => ({
+    currentFormTemplateItemListState: {
+      formTemplateItems: [],
+      term: "",
+      order,
+      orderDir,
+      currentPage: initialPage,
+      lastPage: 1,
+      perPage,
+      totalFormTemplateItems: 5,
+      hideArchived
+    },
+    currentFormTemplateState: {
+      entity: { id: formTemplateId, code: "FT", name: "Form Template" },
+      errors: {}
+    },
+    currentFormTemplateItemState: {
+      entity: {},
+      errors: {}
+    }
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("Component", () => {
+    test("should request page 1 on mount when previous page is 2", async () => {
+      renderWithRedux(
+        <FormTemplateItemListPage formTemplateId={formTemplateId} />,
+        {
+          initialState: buildInitialState()
+        }
+      );
+
+      await waitFor(() => {
+        expect(getFormTemplate).toHaveBeenCalledWith(formTemplateId);
+        expect(getFormTemplateItems).toHaveBeenCalledTimes(1);
+        expect(getFormTemplateItems).toHaveBeenNthCalledWith(
+          1,
+          formTemplateId,
+          "",
+          DEFAULT_CURRENT_PAGE,
+          perPage,
+          order,
+          orderDir,
+          hideArchived
+        );
+      });
+    });
+  });
+});
