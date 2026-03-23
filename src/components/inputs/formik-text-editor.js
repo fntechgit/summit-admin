9608be7ec45f2212fc1df227601d487c1a71220b
Priscila Moneo <priscila_moneo@hotmail.com.ar>
2026-03-23 17:46:08 -0300

    feat: tab navigation forms order (#828)

--- src/components/inputs/formik-text-editor.js
+++ src/components/inputs/formik-text-editor.js
@@ -3,14 +3,16 @@ import TextEditorV3 from "openstack-uicore-foundation/lib/components/inputs/edit
 import { useField } from "formik";
 import normalizeHtmlString from "../../utils/normalize-html-string";
 
-const FormikTextEditor = ({ name, ...props }) => {
+const FormikTextEditor = ({ name, options = {}, ...props }) => {
   const [field, meta, helpers] = useField(name);
+  const mergedOptions = { tabIndex: 0, allowTabNavigation: true, ...options };
 
   return (
     <TextEditorV3
       name={name}
       id={name}
       value={field.value}
+      options={mergedOptions}
       onChange={(e) => {
         const stringValue = normalizeHtmlString(e.target.value);
         helpers.setValue(stringValue);
