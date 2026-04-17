3f4d6120230d8ef6fa7ff5e52d76732536051842
Santiago Palenque <santipalenque@gmail.com>
2026-04-17 17:18:42 -0300

    chore: lazy load components - WIP

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,38 +1,40 @@
-const TerserJSPlugin            = require('terser-webpack-plugin');
-const MiniCssExtractPlugin      = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const { merge }                 = require('webpack-merge');
-const common                    = require('./webpack.common.js');
-const path                      = require('path');
-
+const { merge } = require("webpack-merge");
+const path = require("path");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        filename: '[name]_[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        pathinfo: false
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserJSPlugin({
-                    parallel: true,
-                    terserOptions: {
-                        mangle: { reserved: ['Lock','SuperTokensLock','GET_TOKEN_SILENTLY_LOCK_KEY'] },
-                        compress: {inline: false}
-                    }
-            }),
-            new CssMinimizerPlugin(),
-        ],
-        splitChunks: {
-            chunks: 'all'
+  mode: "production",
+  output: {
+    filename: "[name]_[contenthash].js",
+    chunkFilename: "[name]_[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    pathinfo: false
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserJSPlugin({
+        parallel: true,
+        terserOptions: {
+          mangle: {
+            reserved: ["Lock", "SuperTokensLock", "GET_TOKEN_SILENTLY_LOCK_KEY"]
+          },
+          compress: { inline: false }
         }
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
-    ]
+      }),
+      new CssMinimizerPlugin()
+    ],
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
+  ]
 });
