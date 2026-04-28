2d4ffb29d4155d0fb1c061fa94b110c656009619
Priscila Moneo <priscila_moneo@hotmail.com.ar>
2026-04-28 15:33:20 -0300

    fix: new selection plan issue fix (#865)

--- src/reducers/selection_plans/__tests__/selection-plan-reducer.test.js
+++ src/reducers/selection_plans/__tests__/selection-plan-reducer.test.js
@@ -1,7 +1,37 @@
 import selectionPlanReducer from "../selection-plan-reducer";
-import { SELECTION_PLAN_EVENT_TYPE_ADDED } from "../../../actions/selection-plan-actions";
+import {
+  RESET_SELECTION_PLAN_FORM,
+  SELECTION_PLAN_EVENT_TYPE_ADDED
+} from "../../../actions/selection-plan-actions";
 
 describe("SelectionPlanReducer", () => {
+  describe("RESET_SELECTION_PLAN_FORM", () => {
+    test("should reset hidden flag to false", () => {
+      const initialState = {
+        entity: {
+          id: 1,
+          is_hidden: true,
+          event_types: [],
+          track_groups: [],
+          extra_questions: [],
+          allowed_presentation_action_types: [],
+          track_chair_rating_types: [],
+          marketing_settings: {}
+        },
+        allowedMembers: { data: [], currentPage: 1, lastPage: 1 },
+        errors: {}
+      };
+
+      const result = selectionPlanReducer(initialState, {
+        type: RESET_SELECTION_PLAN_FORM,
+        payload: {}
+      });
+
+      expect(result.entity.id).toBe(0);
+      expect(result.entity.is_hidden).toBe(false);
+    });
+  });
+
   describe("SELECTION_PLAN_EVENT_TYPE_ADDED", () => {
     test("should append event type for selection plan event type added action", () => {
       const initialState = {
