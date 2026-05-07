fc19bf62acb728dc15cf3c6231787377a380f5ff
Santiago Palenque <santipalenque@gmail.com>
2026-05-07 17:56:58 -0300

    chore: building ui - WIP

---
+++ src/components/GridFilter/components/Dropdown.jsx
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+import React from "react";
+import T from "i18n-react/dist/i18n-react";
+import {
+  Select,
+  FormControl,
+  MenuItem,
+  InputLabel
+} from "@mui/material";
+import PropTypes from "prop-types";
+
+
+const Dropdown = ({id, value, options, placeholder, label, onChange, ...rest}) => {
+  const finalPlaceholder =
+    placeholder || T.translate("general.select_an_option");
+
+  return (
+    <FormControl fullWidth>
+      {label && <InputLabel id={`${id}-label`}>{label}</InputLabel>}
+    <Select
+      value={value}
+      label={label}
+      onChange={onChange}
+      labelId={`${id}-label`}
+      displayEmpty
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...rest}
+      renderValue={(selected) => {
+        if (selected == null || selected === "") {
+          return <em>{finalPlaceholder}</em>;
+        }
+        const selectedOption = options.find(
+          ({ value }) => value === selected
+        );
+        return selectedOption ? selectedOption.label : "";
+      }}
+    >
+      {options.map((op) => (
+        <MenuItem key={`selectop-${op.value}`} value={op.value}>
+          {op.label}
+        </MenuItem>
+      ))}
+    </Select>
+    </FormControl>
+  )
+}
+
+Dropdown.propTypes = {
+  id: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    label: PropTypes.string.isRequired,
+  })).isRequired,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+}
+
+Dropdown.defaultProps = {
+  value: null,
+  label: "",
+  placeholder: "",
+}
+
+export default Dropdown;
---
+++ src/components/GridFilter/components/Filter.jsx
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2026 OpenStack Foundation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+import React from "react";
+import { Button, Grid2 } from "react-bootstrap";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Dropdown from "./Dropdown";
+import ValueInput from "./ValueInput";
+
+const Filter = ({ id, value, criteriaSettings, operatorSettings, valueSettings, onChange, onAdd, onDelete }) => {
+  const handleChange = (prop, val) => {
+    onChange({ ...value, [prop]: val });
+  };
+
+  return (
+    <Grid2 container spacing={2} sx={{ alignItems: "center" }}>
+      <Grid2 size={11}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Dropdown
+            id={`${id}-column`}
+            value={value.criteria}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...criteriaSettings}
+            onChange={(val) => handleChange("criteria", val)}
+          />
+          <Dropdown
+            id={`${id}-operator`}
+            value={value.operator}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...operatorSettings}
+            onChange={(val) => handleChange("operator", val)}
+          />
+          <ValueInput
+            id={`${id}-value`}
+            value={value.value}
+            type={valueSettings.type}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...valueSettings.props}
+            onChange={(val) => handleChange("value", val)}
+          />
+        </Box>
+      </Grid2>
+      <Grid2 size={1}>
+        <Button
+          variant="outlined"
+          aria-label="delete"
+          onClick={() => onDelete(id)}
+        >
+          <DeleteIcon />
+        </Button>
+        <Button variant="contained" aria-label="add" onClick={onAdd}>
+          <AddIcon />
+        </Button>
+      </Grid2>
+    </Grid2>
+  );
+};
+
+Filter.propTypes = {
+  id: PropTypes.string,
+  value: PropTypes.shape({
+    criteria: PropTypes.string,
+    operator: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool, PropTypes.array])
+  }),
+  criteriaSettings: PropTypes.shape({
+    options: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      label: PropTypes.string
+    })).isRequired,
+    placeholder: PropTypes.string
+  }).isRequired,
+  operatorSettings: PropTypes.shape({
+    options: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      label: PropTypes.string
+    })).isRequired,
+    placeholder: PropTypes.string
+  }).isRequired,
+  valueSettings: PropTypes.shape({
+    type: PropTypes.string, // class name of the component to render the value
+    props: PropTypes.object, // props to pass to the component
+  }).isRequired,
+  onChange: PropTypes.func.isRequired,
+  onAdd: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired
+};
+
+Filter.defaultProps = {
+  value: null
+};
+
+export default Filter;
