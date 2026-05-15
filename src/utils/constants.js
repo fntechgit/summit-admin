dcb8dadc6f97fcdfa8d8dda586c3be47f73d6c99
Tomás Castillo <tcastilloboireau@gmail.com>
2026-05-15 15:21:47 -0300

    fix: display expanded modules if they're collapsed and have errors on submit
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -138,6 +138,8 @@ export const DEBOUNCE_WAIT = 500;
 
 export const DEBOUNCE_WAIT_250 = 250;
 
+export const DEBOUNCE_WAIT_150 = 150;
+
 export const LETTERS_IN_ALPHABET = 26;
 
 export const UPPERCASE_A_IN_ASCII = 65;
