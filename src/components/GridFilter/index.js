71d77ae6ae6a726764526cfbf386d7cc3d573d89
Santiago Palenque <santipalenque@gmail.com>
2026-05-18 12:00:33 -0300

    chore: move GridFilter to uicore

--- src/components/GridFilter/index.js
+++
@@ -1,5 +0,0 @@
-export { default as GridFilter } from "./GridFilter";
-export { OPERATORS, JOIN_OPERATORS } from "./utils";
-export { default as useGridFilter, EMPTY_FILTER } from "./hooks/useGridFilter";
-export { default as allFiltersReducer } from "./reducers/all-filters-reducer";
-export { saveFilters, SAVE_FILTERS } from "./actions/filter-actions";
