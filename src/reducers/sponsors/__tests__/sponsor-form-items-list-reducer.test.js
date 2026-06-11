e1edafdc8fd52acf3024d026d9382b24a77cd132
Tomás Castillo <tcastilloboireau@gmail.com>
2026-06-11 16:22:26 -0300

    fix: updte test cases
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/reducers/sponsors/__tests__/sponsor-form-items-list-reducer.test.js
+++ src/reducers/sponsors/__tests__/sponsor-form-items-list-reducer.test.js
@@ -310,7 +310,7 @@ describe("SponsorFormItemsListReducer", () => {
       );
       expect(result).toStrictEqual({
         ...initialState,
-        totalCount: 1,
+        totalCount: 2,
         items: [
           {
             id: "A",
@@ -379,7 +379,7 @@ describe("SponsorFormItemsListReducer", () => {
       );
       expect(result).toStrictEqual({
         ...initialState,
-        totalCount: 1,
+        totalCount: 2,
         items: [
           {
             id: "A",
