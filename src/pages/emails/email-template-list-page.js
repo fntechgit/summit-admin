cb7a5d8f58c69a10777849679a372dede64724cc
Tomás Castillo <tcastilloboireau@gmail.com>
2026-09-02 02:19:57 -0300

    fix: restore useState from rebase
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/pages/emails/email-template-list-page.js
+++ src/pages/emails/email-template-list-page.js
@@ -11,7 +11,7 @@
  * limitations under the License.
  * */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
